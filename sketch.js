c24bd8bc5b24288211a9f72d0dc14173b8f352d2
juquia <00001092463483sp@al.educacao.sp.gov.br>
2024-05-27 19:17:14 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,39 +1,36 @@
-let palavra;
-let parcial;
-let quantidade;
+let cor;
+let circuloX;
+let circuloY;
 
 function setup() {
   createCanvas(400, 400);
+  background(color(100,0,0));
+  cor=color(random(0,255),random(0,255),random(0,255));
   
-  palavra = palavraAleatoria();
-  
-}
-
-function palavraAleatoria(){
-   let palavras = ["caminhante", "caminho", "caminha"];
-  return random(palavras);
-}
-
-function inicializaCores (){
-  background("white");
-  fill("black");
-  textSize(64);
-  textAlign(CENTER, CENTER);
-}
-
-function palavraParcial(minimo, maximo){
-  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
-  let parcial = palavra.substring(0, quantidade);
-  return parcial;
+  circuloX= [0,0,0];
+  circuloY= [random(height), random(height), random(height)];
 }
 
 function draw() {
+  fill(cor);
+  circle(circuloX[0],circuloY[0],50);
+  circle(circuloX[1],circuloY[1],50);
+  circle(circuloX[2],circuloY[2],50);
+   
+  for(let contador in circuloX){
+    circle(circuloX[contador], circuloY[contador], 50);
+    circuloX[contador]+= random(0,3);
+    circuloY[contador]+= random(-3,3);
   
-  inicializaCores();
+  if(circuloX[contador] >= width){
+    circuloX[contador] = 0;
+    circuloY[contador] = random(height);
+  }
+    
+  }
   
-  let texto = palavraParcial(0, width);
-  text(texto, 200, 200);
   
+  if(mouseIsPressed){
+    cor=color(random(0,255),random(0,255),random(0,255),random(0,100));
+  }
 }
-
-
